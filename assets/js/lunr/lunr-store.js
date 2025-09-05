03de6d718d4bad4f0eda8e88f603a36a82ec793f
Rongtingting <Rongtingting@users.noreply.github.com>
2025-09-05 18:38:08 +0000

    deploy: 2aa97c00d49bcadfabe6b233ab1f63cc8b74e04d

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Protocol1",
+        "excerpt":"Abstract  A reproducible method for …  ","categories": [],
+        "tags": [],
+        "url": "/docs/protocols/protocol1/",
+        "teaser": null
+      },{
+        "title": "Protocol2",
+        "excerpt":"Abstract  A reproducible method for …  ","categories": [],
+        "tags": [],
+        "url": "/docs/protocols/protocol2/",
+        "teaser": null
+      }]
